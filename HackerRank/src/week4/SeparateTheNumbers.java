c6f391cb9bf9060b6e845ceb2f92dcdf20d7c745
suinjeong <suinjeong8696@gmail.com>
2023-05-12 18:42:34 +0900

    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 참고

---
+++ HackerRank/src/week4/SeparateTheNumbers.java
@@ -0,0 +1,37 @@
+package week4;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class SeparateTheNumbers {
+//https://sowon-dev.github.io/2021/03/01/210302al-hackerrank-Separate-the-Numbers/ 참고
+	
+	public static void separateNumbers(String s) {
+	    // Write your code here
+		String substr = "";
+		boolean isValid = false;
+		
+		for(int i = 1; i<=s.length()/2; i++) {
+			substr = s.substring(0,i);
+			Long subs = Long.parseLong(substr);
+			
+			String validStr = substr;
+			while(validStr.length() < s.length()) {
+				validStr += ++subs;
+			}
+			
+			if(s.equals(validStr)) {
+				isValid = true;
+				break;
+			}
+		}
+	    System.out.println(isValid ? "YES " + substr : "NO");
+	 }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "1234";
+		separateNumbers(s);
+	}
+
+}
