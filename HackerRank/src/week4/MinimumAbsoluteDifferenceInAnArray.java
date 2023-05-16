f253b29ed68e851c741515d3d427a384e45be5d4
suinjeong <suinjeong8696@gmail.com>
2023-05-16 18:35:13 +0900

    05.16

---
+++ HackerRank/src/week4/MinimumAbsoluteDifferenceInAnArray.java
@@ -0,0 +1,30 @@
+package week4;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class MinimumAbsoluteDifferenceInAnArray {
+	
+	public static int minimumAbsoluteDifference(List<Integer> arr) {
+	    // Write your code here
+		Collections.sort(arr);
+		int d = arr.get(1)-arr.get(0);
+		
+		for(int i = 0; i<arr.size()-1; i++) {
+			if(d > arr.get(i+1)-arr.get(i)) {
+				d = arr.get(i+1)-arr.get(i);
+			}
+		}
+		
+	    return d;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> arr = Arrays.asList(1, -3, 71, 68, 17);
+		int result = minimumAbsoluteDifference(arr);
+		System.out.println(result);
+	}
+
+}
--- HackerRank/src/week4/TowerBreakers.java
+++ HackerRank/src/week4/TowerBreakers.java
@@ -1,6 +1,11 @@
 package week4;
 
 public class TowerBreakers {
+	
+	public static int towerBreakers(int n, int m) {
+	    // Write your code here
+	        return n%2 == 0 || m==1 ? 2 : 1;
+	    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
