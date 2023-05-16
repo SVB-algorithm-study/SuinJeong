db3eebf7efc9f0736e9a4773e8bc697c0ab9b0b1
suinjeong <suinjeong8696@gmail.com>
2023-05-16 18:55:04 +0900

    05.16

--- HackerRank/src/week4/TowerBreakers.java
+++ HackerRank/src/week4/TowerBreakers.java
@@ -5,7 +5,7 @@ public class TowerBreakers {
 	public static int towerBreakers(int n, int m) {
 	    // Write your code here
 	        return n%2 == 0 || m==1 ? 2 : 1;
-	    }
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
